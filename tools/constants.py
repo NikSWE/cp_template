698e8e37c86648ae5ca4f90d1f4b62077fbd4652
muj-programmer <nick.muj.programmer@gmail.com>
2019-08-24 17:41:02 +0530

    :tada: initial commit

---
+++ tools/constants.py
@@ -0,0 +1,11 @@
+# Commit Emojis
+EMOJI_AC = ':white_check_mark:'
+EMOJI_WA = ':x:'
+EMOJI_TLE = ':exclamation:'
+EMOJI_INC = ':question:'
+
+# Commit Messages
+MSG_AC = 'accepted solution'
+MSG_WA = 'wrong solution'
+MSG_TLE = 'optimize solution'
+MSG_INC = 'incomplete solution'
