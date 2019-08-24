bb41aa11768f60147c285eccc148f4fb16619e40
muj-programmer <nick.muj.programmer@gmail.com>
2019-08-24 22:09:01 +0530

    :wrench: fixed default file comment symbol

--- tools/create_solution_file.py
+++ tools/create_solution_file.py
@@ -1,19 +1,34 @@
-from argparse import ArgumentParser
+from argparse import ArgumentParser, ArgumentError
 from pathlib import Path
 from os import path
 from datetime import datetime
+import constants as const
 
 now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-CONTENTS = ["# PVT", " ", f"{now} IST #\n"]
 
 parser = ArgumentParser()
 parser.add_argument("dir", help="create a file at this directory.")
 parser.add_argument("file", help="file name to create with extension.")
+parser.add_argument("-s",
+                    "--symbol",
+                    help="comment symbol for the file extension")
 args = parser.parse_args()
 
-dir_path = path.join(path.dirname(path.abspath(__file__)), f"../{args.dir}")
+file_name, file_extension = path.splitext(args.file)
+
+if file_extension[1:] != const.LANG_EXT and args.symbol == None:
+    parser.error(f"provide comment symbol for `{file_extension}` extension through [-s] flag")
+
+symbol = None
+if file_extension[1:] == const.LANG_EXT:
+    symbol = const.LANG_COMMENT_SYMBOL
+else:
+    symbol = args.symbol
+CONTENTS = [f"{symbol} PVT", " ", f"{now} IST {symbol}\n"]
+
+dir_path = path.join(const.REPO_PATH, f"{args.dir}")
 Path(dir_path).mkdir(parents=True, exist_ok=True)
 
-file_path = dir_path + f"/{args.file}"
+file_path = dir_path + f"/{file_name}{file_extension}"
 with open(file_path, 'w') as target:
     target.writelines(CONTENTS)
