18e643fd202dfe95b11134d0cd4f5efc8e6a1870
muj-programmer <nick.muj.programmer@gmail.com>
2019-08-24 22:06:16 +0530

    :sparkles: added new constants

--- tools/constants.py
+++ tools/constants.py
@@ -4,6 +4,10 @@ from os import path
 REPO_PATH = path.dirname(path.dirname(path.abspath(__file__)))
 LOG_PATH = path.join(REPO_PATH, "tools/logs")
 
+# programming lang
+LANG_EXT = 'py'
+LANG_COMMENT_SYMBOL = '#'
+
 # commit emojis
 EMOJI_AC = ':white_check_mark:'
 EMOJI_WA = ':x:'
