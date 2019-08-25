8501ef8eef823685e555e455724cff7529a2985d
muj-programmer <nick.muj.programmer@gmail.com>
2019-08-25 15:13:03 +0530

    :heavy_plus_sign: remove log files

--- tools/create_alias.py
+++ tools/create_alias.py
@@ -2,5 +2,8 @@ import constants as const
 
 csf_cmd = 'python3 {}/tools/create_solution_file.py'.format(const.REPO_PATH)
 psf_cmd = 'python3 {}/tools/commit_files.py'.format(const.REPO_PATH)
+rmlf_cmd = 'python3 {}/tools/remove_log_files.py'.format(const.REPO_PATH)
+
 print(f'alias csf="{csf_cmd}"')
 print(f'alias psf="{psf_cmd}"')
+print(f'alias rmlf="{rmlf_cmd}"')
