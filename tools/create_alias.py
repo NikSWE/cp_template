749166509b9ec285e299cba2ef0cfeb68e253902
muj-programmer <nick.muj.programmer@gmail.com>
2019-08-25 12:53:31 +0530

    :wrench: fixed alias syntax

--- tools/create_alias.py
+++ tools/create_alias.py
@@ -2,5 +2,5 @@ import constants as const
 
 csf_cmd = 'python3 {}/tools/create_solution_file.py'.format(const.REPO_PATH)
 psf_cmd = 'python3 {}/tools/commit_files.py'.format(const.REPO_PATH)
-print(f'alias csf={csf_cmd}')
-print(f'alias psf={psf_cmd}')
+print(f'alias csf="{csf_cmd}"')
+print(f'alias psf="{psf_cmd}"')
